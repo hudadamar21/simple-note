611df89844b6864233c67dca3bda34e456910318
hudadamar21 <hudadamar21@gmail.com>
2020-04-13 08:18:41 +0700

    Updates

---
+++ precache-manifest.dcdd42823efafea7df8f1789b2125c89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ca0251c46eb14d16e439fd319b70b3",
+    "url": "/simple-note/index.html"
+  },
+  {
+    "revision": "ac506deb6fb3fc3d3a9a",
+    "url": "/simple-note/static/css/main.37cfd6c1.chunk.css"
+  },
+  {
+    "revision": "32808fea8fdab61327c3",
+    "url": "/simple-note/static/js/2.0c861bc8.chunk.js"
+  },
+  {
+    "revision": "73138c634db8d37252cbabcb33689813",
+    "url": "/simple-note/static/js/2.0c861bc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac506deb6fb3fc3d3a9a",
+    "url": "/simple-note/static/js/main.4778f5be.chunk.js"
+  },
+  {
+    "revision": "a14fdf5233e7c8364dfa",
+    "url": "/simple-note/static/js/runtime-main.87ce10be.js"
+  }
+]);
